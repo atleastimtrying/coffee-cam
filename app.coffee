90f5934837a8da524a05cc47671cc53a995a5614
Anders Fisher <atleastimtrying@gmail.com>
2012-06-16 22:00:02 +0100

    video element now excluded from the dom, using canvas element now

--- app.coffee
+++ app.coffee
@@ -1,15 +1,33 @@
-class App
-	constructor: ->
+class Display
+	constructor: (@video)->
+		@canvas = $('canvas')[0]
+		@canvas.width = 640
+		@canvas.height = 480
+		@context = @canvas.getContext '2d'
+		@context.globalAlpha = 0.1
+	draw: =>
+		@context.fillRect 320,0,320,480
+		@context.drawImage @video, 0, 0, 640, 480
+		window.requestAnimationFrame @draw
+
+class Source
+	constructor: (@video)->
 		@navigator = navigator
-		@display = $('#display')[0]
-		@display.autoplay = true
 		@navigator.webkitGetUserMedia
 			video: true
-			audio: true
-		, 
-		@success, 
-		@error
-	success: (stream)-> @display.src = window.webkitURL.createObjectURL stream
+			audio: false
+		, @success, @error
+	
+	success: (stream)=> 
+		@video.src = window.webkitURL.createObjectURL stream
+		window.app.display.draw()
+	
 	error: -> alert "broked"
+
+class App
+	constructor: ->
+		@video = $('<video autoplay></video>')[0]
+		@source = new Source @video
+		@display = new Display @video
 
 $ -> window.app = new App
--- app.js
+++ app.js
@@ -1,25 +1,61 @@
 (function() {
-  var App;
+  var App, Display, Source;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  Display = (function() {
+
+    function Display(video) {
+      this.video = video;
+      this.draw = __bind(this.draw, this);
+      this.canvas = $('canvas')[0];
+      this.canvas.width = 640;
+      this.canvas.height = 480;
+      this.context = this.canvas.getContext('2d');
+      this.context.globalAlpha = 0.1;
+    }
+
+    Display.prototype.draw = function() {
+      this.context.fillRect(320, 0, 320, 480);
+      this.context.drawImage(this.video, 0, 0, 640, 480);
+      return window.requestAnimationFrame(this.draw);
+    };
+
+    return Display;
+
+  })();
+
+  Source = (function() {
+
+    function Source(video) {
+      this.video = video;
+      this.success = __bind(this.success, this);
+      this.navigator = navigator;
+      this.navigator.webkitGetUserMedia({
+        video: true,
+        audio: false
+      }, this.success, this.error);
+    }
+
+    Source.prototype.success = function(stream) {
+      this.video.src = window.webkitURL.createObjectURL(stream);
+      return window.app.display.draw();
+    };
+
+    Source.prototype.error = function() {
+      return alert("broked");
+    };
+
+    return Source;
+
+  })();
 
   App = (function() {
 
     function App() {
-      this.navigator = navigator;
-      this.display = $('#display')[0];
-      this.display.autoplay = true;
-      this.navigator.webkitGetUserMedia({
-        video: true,
-        audio: true
-      }, this.success, this.error);
+      this.video = $('<video autoplay></video>')[0];
+      this.source = new Source(this.video);
+      this.display = new Display(this.video);
     }
-
-    App.prototype.success = function(stream) {
-      return this.display.src = window.webkitURL.createObjectURL(stream);
-    };
-
-    App.prototype.error = function() {
-      return alert("broked");
-    };
 
     return App;
 
