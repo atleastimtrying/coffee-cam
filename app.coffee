c17578aaa5eaa9198cb67583d73dd8978f5350a0
Anders Fisher <atleastimtrying@gmail.com>
2012-06-17 21:10:50 +0100

    strating to get into filters

--- app.coffee
+++ app.coffee
@@ -1,33 +1,58 @@
 class Display
-	constructor: (@video)->
-		@canvas = $('canvas')[0]
-		@canvas.width = 640
-		@canvas.height = 480
-		@context = @canvas.getContext '2d'
-		@context.globalAlpha = 0.1
-	draw: =>
-		@context.fillRect 320,0,320,480
-		@context.drawImage @video, 0, 0, 640, 480
-		window.requestAnimationFrame @draw
+  constructor: (@video)->
+    @canvas = $('canvas')[0]
+    @canvas.width = 320
+    @canvas.height = 240
+    @context = @canvas.getContext '2d'
+  
+  draw: (pixelData)=>
+    pixelArray = pixelData.data
+    @pixelChange pixelArray, index for pixel, index in pixelArray
+    pixelData.data = pixelArray
+    @context.putImageData pixelData, 0, 0
+  
+  pixelChange: (array, index)->
+    r = array[index]
+    g = array[index+1]
+    b = array[index+2]
+    a = array[index+3]
+    brightness = (r+g+b)/3
+    array[index] = r
+    array[index+1] = g
+    array[index+2] = b
+    array[index+3] = a
 
 class Source
-	constructor: (@video)->
-		@navigator = navigator
-		@navigator.webkitGetUserMedia
-			video: true
-			audio: false
-		, @success, @error
-	
-	success: (stream)=> 
-		@video.src = window.webkitURL.createObjectURL stream
-		window.app.display.draw()
-	
-	error: -> alert "broked"
+  constructor: (@video)->
+    @navigator = navigator
+    @navigator.webkitGetUserMedia
+      video: true
+      audio: false
+    , @success, @error
+    @canvas = document.createElement 'canvas'
+    @canvas.width = 320
+    @canvas.height = 240
+    @context = @canvas.getContext '2d'
+  
+  success: (stream)=> 
+    @video.src = window.webkitURL.createObjectURL stream
+    window.app.draw()
+  
+  getVideoPixels: ->
+    @context.drawImage @video, 0, 0 , 320, 240
+    #@context.fillRect 10,10,100,100
+    iData = @context.getImageData 0, 0, 320, 240
+    iData
+
+  error: -> alert "broked"
 
 class App
-	constructor: ->
-		@video = $('<video autoplay></video>')[0]
-		@source = new Source @video
-		@display = new Display @video
+  constructor: ->
+    @video = $('<video autoplay></video>')[0]
+    @source = new Source @video
+    @display = new Display @video
+  draw: =>
+    @display.draw @source.getVideoPixels()
+    window.requestAnimationFrame @draw
 
 $ -> window.app = new App
--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 (function() {
   var App, Display, Source;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
   Display = (function() {
-
     function Display(video) {
       this.video = video;
       this.draw = __bind(this.draw, this);
       this.canvas = $('canvas')[0];
-      this.canvas.width = 640;
-      this.canvas.height = 480;
+      this.canvas.width = 320;
+      this.canvas.height = 240;
       this.context = this.canvas.getContext('2d');
-      this.context.globalAlpha = 0.1;
     }
-
-    Display.prototype.draw = function() {
-      this.context.fillRect(320, 0, 320, 480);
-      this.context.drawImage(this.video, 0, 0, 640, 480);
-      return window.requestAnimationFrame(this.draw);
+    Display.prototype.draw = function(pixelData) {
+      var index, pixel, pixelArray, _len;
+      pixelArray = pixelData.data;
+      for (index = 0, _len = pixelArray.length; index < _len; index++) {
+        pixel = pixelArray[index];
+        this.pixelChange(pixelArray, index);
+      }
+      pixelData.data = pixelArray;
+      return this.context.putImageData(pixelData, 0, 0);
     };
-
+    Display.prototype.pixelChange = function(array, index) {
+      var a, b, brightness, g, r;
+      r = array[index];
+      g = array[index + 1];
+      b = array[index + 2];
+      a = array[index + 3];
+      brightness = (r + g + b) / 3;
+      array[index] = r;
+      array[index + 1] = g;
+      array[index + 2] = b;
+      return array[index + 3] = a;
+    };
     return Display;
-
   })();
-
   Source = (function() {
-
     function Source(video) {
       this.video = video;
       this.success = __bind(this.success, this);
@@ -34,35 +43,39 @@
         video: true,
         audio: false
       }, this.success, this.error);
+      this.canvas = document.createElement('canvas');
+      this.canvas.width = 320;
+      this.canvas.height = 240;
+      this.context = this.canvas.getContext('2d');
     }
-
     Source.prototype.success = function(stream) {
       this.video.src = window.webkitURL.createObjectURL(stream);
-      return window.app.display.draw();
+      return window.app.draw();
     };
-
+    Source.prototype.getVideoPixels = function() {
+      var iData;
+      this.context.drawImage(this.video, 0, 0, 320, 240);
+      iData = this.context.getImageData(0, 0, 320, 240);
+      return iData;
+    };
     Source.prototype.error = function() {
       return alert("broked");
     };
-
     return Source;
-
   })();
-
   App = (function() {
-
     function App() {
-      this.video = $('<video autoplay></video>')[0];
+      this.draw = __bind(this.draw, this);      this.video = $('<video autoplay></video>')[0];
       this.source = new Source(this.video);
       this.display = new Display(this.video);
     }
-
+    App.prototype.draw = function() {
+      this.display.draw(this.source.getVideoPixels());
+      return window.requestAnimationFrame(this.draw);
+    };
     return App;
-
   })();
-
   $(function() {
     return window.app = new App;
   });
-
 }).call(this);
